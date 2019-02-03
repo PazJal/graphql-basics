3e4d794852cead5500e5a8542b1fd0b6eac5a719
Paz Jaldety <paz@coperato.local>
2019-02-03 17:33:19 +0200

    Added mutation for updating the data types.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -103,6 +103,28 @@ const Mutation = {
     return deletedPosts[0];
   },
 
+  updatePost(parent, args, {db}, info) {
+    const {id, data} = args;
+    const post = db.posts.find((post) => post.id === id);
+    if(!post) {
+      throw new Error(`Post ${id} not found.`);
+    }
+
+    if(typeof data.title === 'string'){
+      post.title = data.title;
+    }
+
+    if(typeof data.body === 'string'){
+      post.body = data.body;
+    }
+
+    if(typeof data.published === 'boolean'){
+      post.published = data.published;
+    }
+
+    return post;
+  },
+
   createComment(parent, args, {db}, info){
     const {author, post} = args.data;
     const userExists = db.users.some((user) => user.id === author);
@@ -136,6 +158,21 @@ const Mutation = {
     const deletedComments = db.comments.splice(commentIndex , 1);
 
     return deletedComments[0];
+  },
+
+  updateComment(parent, args, {db}, info){
+    const {id, data} = args;
+    const comment = db.comments.find((comment) => comment.id === id);
+
+    if(!comment) {
+      throw new Error(`Comment ${id} not found`);
+    }
+
+    if(typeof data.text === 'string'){
+      comment.text = data.text;
+    }
+
+    return comment;
   }
 }
 
